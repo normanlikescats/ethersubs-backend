05ad216e9c604ab91123fdc0f1264df62f342a41
normanlikescats <normankwok97@gmail.com>
2023-05-08 09:09:52 +0800

    code dependency deletions

--- Controllers/CreatorController.js
+++ Controllers/CreatorController.js
@@ -1,6 +1,10 @@
 class CreatorController {
-  constructor(creatorModel) {
+  constructor(creatorModel, postModel, commentModel, followModel, transactionModel) {
     this.creatorModel = creatorModel;
+    this.postModel = postModel;
+    this.commentModel = commentModel;
+    this.followModel = followModel;
+    this.transactionModel = transactionModel;
   }
 
   // Grab all Creator Profiles
@@ -121,8 +125,100 @@ class CreatorController {
   // Delete Creator Profile
   deleteCreator = async (req, res) => {
     const id = req.params.id
-    // code in dependency deletion here
+    let postIdArr;
+    let commentIdArr;
+    let followIdArr;
+    let transactionIdArr;
+    // search for posts
     try{
+      const postId = await this.postModel.findAll({
+        attributes: ['id'],
+        where:{
+          creator_id: id
+        }
+      })
+      let stringifiedPostId = JSON.stringify(postId)
+      let parsedPostId = JSON.parse(stringifiedPostId)
+      postIdArr = parsedPostId.map(id => id.id)
+    } catch(err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+    // search for comments
+    try{
+      const commentId = await this.commentModel.findAll({
+        attributes: ['id'],
+        where:{
+          post_id: postIdArr
+        }
+      })
+      let stringifiedCommentId = JSON.stringify(commentId)
+      let parsedCommentId = JSON.parse(stringifiedCommentId)
+      commentIdArr = parsedCommentId.map(id => id.id)
+    } catch(err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+    // search for follows
+    try{
+      const followsId = await this.followModel.findAll({
+        attributes: ['id'],
+        where:{
+          creator_id: id
+        }
+      })
+      let stringifiedFollowId = JSON.stringify(followsId)
+      let parsedFollowId = JSON.parse(stringifiedFollowId)
+      followIdArr = parsedFollowId.map(id => id.id)
+    } catch(err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+    // search for transactions
+    try{
+      const transactionId = await this.transactionModel.findAll({
+        attributes: ['id'],
+        where:{
+          creator_id: id
+        }
+      })
+      let stringifiedTransactionId = JSON.stringify(transactionId)
+      let parsedTranasctionId = JSON.parse(stringifiedTransactionId)
+      transactionIdArr = parsedTranasctionId.map(id => id.id)
+    } catch(err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+    try{
+      // delete comments
+      await this.commentModel.destroy(
+        {
+          where:{
+            id: commentIdArr
+          }
+        }
+      )
+      // delete posts
+      await this.postModel.destroy(
+        {
+          where:{
+            id: postIdArr
+          }
+        }
+      )
+      // delete follows
+      await this.followModel.destroy(
+        {
+          where:{
+            id: followIdArr
+          }
+        }
+      )
+      // delete transactions
+      await this.transactionModel.destroy(
+        {
+          where:{
+            id: transactionIdArr
+          }
+        }
+      )
+      // delete creator
       await this.creatorModel.destroy(
         {
           where:{
